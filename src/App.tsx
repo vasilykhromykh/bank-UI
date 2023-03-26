b8851a904de30034a81faa5b13b87ad18e44a2c9
vasilykhromykh <vasilykhromykh@gmial.vom>
2023-03-26 16:18:28 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import First from './First/First';
+import Second from "./Second/Second";
+import ArticleOne from "./ArticleOne/Article";
+import Header from "./Header/Header";
+import Footer from './Footer/Footer';
+import Business from "./Business/Business";
+import Sticker from "./Sticker/Sticker";
+import Download from "./Download/Download";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+<First/>
+        <Second/>
+        <Business/>
+        <ArticleOne  img='UI/invest.png' left={0} header='Invest!' text='orem ipsum dolor sit amet, consectetur adipiscing elit. Nunc auctor dolor ipsum, sit amet hendrerit ante finibus in. Suspendisse accumsan nisl dictum urna mattis consectetur.'/>
+
+        <Sticker/>
+        <ArticleOne  img='UI/nine.png' left={1} header='9% CashBack' text='orem ipsum dolor sit amet, consectetur adipiscing elit. Nunc auctor dolor ipsum, sit amet hendrerit ante finibus in. Suspendisse accumsan nisl dictum urna mattis consectetur.'/>
+
+        <Download/>
+
+
+
+<Footer/>
+
     </div>
   );
 }
